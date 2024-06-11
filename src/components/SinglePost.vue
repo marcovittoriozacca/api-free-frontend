3f609506bbf379e710f2c7d7e3c50dc03d28a8d6
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-11 09:58:38 +0200

    feat: posts list

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+const props = defineProps(['post']);
+
+
+
+</script>
+
+<template>
+    <div class="border">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.content }}</p>
+        <img :src="post.image? post.image : '/not-found.jpg' " :alt="post.title" class="w-80">
+        <span>{{ post.published? "Published" : "Not published yet" }}</span>
+        <h6>{{ post.category }}</h6>
+        <h6>{{ post.userId }}</h6>
+        <div>
+            <span v-for="(tag, index) in post.tags" :key="index">
+                {{ tag }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,30 @@
 <script setup>
+import axios from 'axios';
+import { onMounted } from 'vue';
+import { store } from '../store.js';
+import SinglePost from '../components/SinglePost.vue';
+
+const getAllPosts = async () => {
+    try{
+        const posts = await axios.get("http://localhost:3000/posts");
+        store.postsList = posts.data;
+    }catch(err){
+        console.error(err);
+    }
+};
+onMounted(() => {
+  getAllPosts();  
+})
+
 
 </script>
 
 <template>
-    <div class="text-center py-5">
-        Hello World 
+    <div class="flex flex-col items-center">
+        <SinglePost 
+            v-for="(post, index) in store.postsList.postsList" :key="post.id"
+            :post = "post"
+        />
     </div>
 </template>
 
