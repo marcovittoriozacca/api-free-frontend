84aa7091bae889b3d0a7ebbd1359b07eee500ba5
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-11 10:09:15 +0200

    feat: env file

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,8 +5,10 @@ import { store } from '../store.js';
 import SinglePost from '../components/SinglePost.vue';
 
 const getAllPosts = async () => {
+    const apiUrl = import.meta.env.VITE_POSTS_SERVER;
+    const postsRoute = import.meta.env.VITE_POSTS;
     try{
-        const posts = await axios.get("http://localhost:3000/posts");
+        const posts = await axios.get(`${apiUrl}/${postsRoute}`);
         store.postsList = posts.data;
     }catch(err){
         console.error(err);
