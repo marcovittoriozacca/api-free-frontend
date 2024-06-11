c11d2dd51ad46b46689938418382c65668ff2ab7
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-11 09:21:55 +0200

    feat: vue router with simple home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import HomePage from './pages/HomePage.vue';
+
 
 </script>
 
 <template>
+
+  <main>
+    <router-view/>
+  </main>
 
 </template>
 
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="text-center py-5">
+        Hello World 
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
